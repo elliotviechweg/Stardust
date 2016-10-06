86194dc1326a15fb3e77650b5881ff1967164612
RNPGhost <samhutton@hotmail.co.uk>
2016-10-06 13:48:59 +0100

    Added a basic main menu
    Added first pass main menu, without the level introduction stage

---
+++ Assets/Scripts/GameObjects/CLevelLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CLevelLoader : MonoBehaviour
+{
+	public string m_sLevelName;
+
+	private void OnTouchDown () {
+		SceneManager.LoadScene(m_sLevelName);
+	}
+}
