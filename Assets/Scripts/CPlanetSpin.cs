dd66a89f32e95840c705de4dcf891451688809a2
Kern.Nembhard <darkone@knembhard.com>
2016-10-07 17:06:06 +0100

    Updated the art assets, new script for spinning the planet and clouds.

---
+++ Assets/Scripts/CPlanetSpin.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CPlanetSpin : MonoBehaviour
+{
+    public float m_fRotationalSpeed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, -Time.deltaTime * m_fRotationalSpeed, 0 );
+    }
+}
