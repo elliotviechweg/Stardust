33d3b3a6381b8761b29a7cf37988252812cbd1d0
RNPGhost <samhutton@hotmail.co.uk>
2016-10-10 09:55:46 +0100

    Removed changes to planet spin

--- Assets/Scripts/GameObjects/CPlanetSpin.cs
+++ Assets/Scripts/GameObjects/CPlanetSpin.cs
@@ -5,11 +5,6 @@ public class CPlanetSpin : MonoBehaviour
 {
     public float m_fRotationalSpeed;
 
-    void Start()
-    {
-		// TODO: use rigidbody.angularVelocity here instead of constant update calls
-    }
-
     void Update()
     {
         transform.Rotate(0, -Time.deltaTime * m_fRotationalSpeed, 0);
