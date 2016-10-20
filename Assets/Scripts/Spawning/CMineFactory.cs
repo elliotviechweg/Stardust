2741b2777b618e5e9b0fe3055868857e4ebbaf40
RNPGhost <samhutton@hotmail.co.uk>
2016-10-20 16:58:22 +0100

    Added mine spawn button

---
+++ Assets/Scripts/Spawning/CMineFactory.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CMineFactory : MonoBehaviour
+{
+	private GameObject m_tMinePrefab;
+	private float m_fMineRadius;
+
+	public void SpawnMine(Vector3 i_vPosition)
+	{
+		// Check area is clear before spawning
+		if (!Physics.CheckSphere(i_vPosition, m_fMineRadius))
+		{
+			GameObject tNewMine = (GameObject)Instantiate(m_tMinePrefab, i_vPosition, new Quaternion(0, 0, 0, 0));
+		}
+	}
+
+	private void Start()
+	{
+		m_tMinePrefab = (GameObject)Resources.Load("Prefabs/Mine");
+
+		// Get Mine radius
+		float fMineRadius = -1;
+		SphereCollider[] tColliders = m_tMinePrefab.GetComponents<SphereCollider>();
+		foreach (SphereCollider tCollider in tColliders)
+		{
+			if (!tCollider.isTrigger)
+			{
+				m_fMineRadius = tCollider.radius;
+				break;
+			}
+		}
+		Debug.Assert(fMineRadius >= 0, "CMineFactory::Start: Object collider not found");
+	}
+}
---
+++ Assets/Scripts/Spawning/CMineSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CMineSpawner : MonoBehaviour
+{
+	public List<Vector3> m_vSpawnPositions;
+
+	public CMineFactory m_tMineFactory;
+	public Transform m_tPlanet;
+		
+	public void SpawnMines()
+	{
+		foreach (Vector3 vSpawnPosition in m_vSpawnPositions)
+		{
+			m_tMineFactory.SpawnMine(GenerateSpawnPosition(vSpawnPosition));
+		}			
+	}
+
+	private Vector3 GenerateSpawnPosition(Vector3 i_vSpawnPosition)
+	{
+		return m_tPlanet.position + i_vSpawnPosition;
+	}
+}
